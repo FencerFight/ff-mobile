bf498244d3f5df1abdb700e9657260416fefcd23
ArtemPodloboshnikov <ArtemPS777@yandex.ru>
2025-08-21 17:16:41 +0300

    fix(rating)

--- app/(tabs)/rating.tsx
+++ app/(tabs)/rating.tsx
@@ -1,5 +1,5 @@
 // app/(drawer)/rating.tsx
-import RatingPage from '@/components/RatingPage ';
+import RatingPage from '@/components/RatingPage';
 import TabSwitcher from '@/components/TabSwitcher';
 import { FG } from '@/constants';
 import { ChartSpline, Medal } from 'lucide-react-native';
@@ -8,7 +8,6 @@ import React from 'react';
 
 export default function RatingScreen() {
   const tabs = [<ChartSpline key={0} size={20} color={FG} />, <Medal key={1} size={20} color={FG} />]
-
 
   return (
     <TabSwitcher
